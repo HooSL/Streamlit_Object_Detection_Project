11b3b54977f234b04752a7db95db23049b024cbe
HooSL <leegntmd@naver.com>
2021-12-31 17:21:47 +0900

    12-31

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import os
 from datetime import datetime
 import numpy as np
 from centernet_model_app import run_cunternet_model_app
+from fasterR_cnn_model_app import run_faster_model_app
 
 from ssdmodel_app import run_ssdmodel_app
 
@@ -21,24 +22,27 @@ def save_uploaded_file(directory,file):
         f.write(file.getbuffer())
     return st.success('Saved file : {} in {}'.format(file.name,directory))
 
-
+title_img = Image.open('data/1.png')
+st.set_page_config(page_title='Image Detection',page_icon=title_img,
+                    layout='wide',initial_sidebar_state='collapsed')
 
 
 def main():
-    st.title('Tensorflow Object Detection')
+    st.title('Tensorflow Object Detection Project')
 
-
+    logo = Image.open('data/tensorflow.png')
+    st.sidebar.image(logo)
     
-    tf_models = ['SSD MobileNet','CenterNet HourGlass','EfficientDet']
+    tf_models = ['SSD MobileNet','CenterNet HourGlass','EfficientDet','Faster R-CNN ResNet']
 
         
     model_choice = st.sidebar.selectbox('TensorFlow Model Select',tf_models)
-    boxes = st.sidebar.slider('Max Boxes',1,200,value=50)
-    min_score = st.sidebar.slider('Score',1,100,value=30)
+    boxes = st.sidebar.slider('Max Boxes',0,200,value=50,step=5)
+    min_score = st.sidebar.slider('Score',1,100,value=50)
 
     if model_choice == 'SSD MobileNet':
 
-        image_file = st.file_uploader('이미지를 업로드 하세요',type=['png','jpg','jpeg'])
+        image_file = st.file_uploader('Upload your image',type=['png','jpg','jpeg'])
         if image_file is not None :
           
 
@@ -60,10 +64,11 @@ def main():
             img = np.array(img)
             #넘파이 어레이를 오브젝트 디택션함수에 넘겨준다
             run_ssdmodel_app(img,boxes,min_score)
+            st.success('Detection Success!')
 
 
     elif model_choice == 'CenterNet HourGlass':
-        image_file = st.file_uploader('이미지를 업로드 하세요',type=['png','jpg','jpeg'])
+        image_file = st.file_uploader('Upload your image',type=['png','jpg','jpeg'])
         if image_file is not None :
             #프린트문은 디버깅용 터미널에 출력된다.
             
@@ -85,9 +90,10 @@ def main():
             img = np.array(img)
             #넘파이 어레이를 오브젝트 디택션함수에 넘겨준다
             run_cunternet_model_app(img,boxes,min_score)
+            st.success('Detection Success!')
 
     elif model_choice == 'EfficientDet':
-        image_file = st.file_uploader('이미지를 업로드 하세요',type=['png','jpg','jpeg'])
+        image_file = st.file_uploader('Upload your image',type=['png','jpg','jpeg'])
         if image_file is not None :
             #프린트문은 디버깅용 터미널에 출력된다.
             
@@ -109,8 +115,32 @@ def main():
             img = np.array(img)
             #넘파이 어레이를 오브젝트 디택션함수에 넘겨준다
             run_cunternet_model_app(img,boxes,min_score)
+            st.success('Detection Success!')
 
+    elif model_choice == 'Faster R-CNN ResNet':
+        image_file = st.file_uploader('Upload your image',type=['png','jpg','jpeg'])
+        if image_file is not None :
+            #프린트문은 디버깅용 터미널에 출력된다.
+            
+            #파일명 변경 후 저장
+            image_file.name = 'test.jpg'
 
+            #파일명을 현재시간의 조합으로 해서 만들어보세요.(안겹치려고)
+            #예) 현재시간.jpg
+            current_time = datetime.now()
+            current_time = current_time.isoformat().replace(':','_')
+            image_file.name = current_time+'.jpg'
+
+            #파일을 저장할 수 있도록 위의 함수를 호출하자
+            #save_uploaded_file('temp',image_file)
+            
+            #오브젝트 디텍션을 여기서 한다
+            img = Image.open(image_file)
+
+            img = np.array(img)
+            #넘파이 어레이를 오브젝트 디택션함수에 넘겨준다
+            run_faster_model_app(img,boxes,min_score)
+            st.success('Detection Success!')
 
 if __name__ == '__main__':
     main()
---
+++ fasterR_cnn_model_app.py
@@ -0,0 +1,106 @@
+import tensorflow as tf
+import os
+import pathlib
+
+import numpy as np
+import zipfile
+
+import matplotlib.pyplot as plt
+from PIL import Image
+
+import cv2
+
+from object_detection.utils import ops as utils_ops
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as viz_utils
+
+import streamlit as st
+
+
+#내 로컬에 설치된 레이블 파일을, 인덱스와 연결시킨다
+
+
+PATH_TO_LABELS = 'C:\\Users\\5-1\\Documents\\TensorFlow\\models\\research\\object_detection\\data\\mscoco_label_map.pbtxt'
+category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS,use_display_name=True)
+
+print(category_index)
+
+#모델 로드하는 함수
+#https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/tf2_detection_zoo.md
+#위 링크에서 모델을 가져올 수 있다
+
+
+#테스트한 모델들
+#/20200713/centernet_hg104_1024x1024_coco17_tpu-32.tar.gz
+#/20200711/faster_rcnn_resnet50_v1_1024x1024_coco17_tpu-8.tar.gz
+
+# Download and extract model
+def download_model(model_name, model_date):
+    base_url = 'http://download.tensorflow.org/models/object_detection/tf2/' #경로는 변하지 않음
+    model_file = model_name + '.tar.gz'
+    model_dir = tf.keras.utils.get_file(fname=model_name,
+                                        origin=base_url + model_date + '/' + model_file,
+                                        untar=True)
+    return str(model_dir)
+#모델 날짜와 모델 이름만 바뀜
+MODEL_DATE = '20200711'
+MODEL_NAME = 'faster_rcnn_resnet50_v1_1024x1024_coco17_tpu-8'
+PATH_TO_MODEL_DIR = download_model(MODEL_NAME, MODEL_DATE)
+
+
+
+def load_model(model_dir):
+    model_full_dir = model_dir + "/saved_model"
+
+    # Load saved model and build the detection function
+    detection_model = tf.saved_model.load(model_full_dir)
+    return detection_model
+
+detection_model = load_model(PATH_TO_MODEL_DIR)
+
+# print(detection_model.signatures['serving_default'].inputs)
+# print()
+# print(detection_model.signatures['serving_default'].output_dtypes)
+# print()
+# print(detection_model.signatures['serving_default'].output_shapes)
+# print()
+
+def show_inference(detection_model,image_np,boxes,min_score) :
+
+    # The input needs to be a tensor, convert it using `tf.convert_to_tensor`.
+    input_tensor = tf.convert_to_tensor(image_np)
+    # The model expects a batch of images, so add an axis with `tf.newaxis`.
+    input_tensor = input_tensor[tf.newaxis, ...]
+
+    # input_tensor = np.expand_dims(image_np, 0)
+    detections = detection_model(input_tensor)
+
+    # All outputs are batches tensors.
+    # Convert to numpy arrays, and take index [0] to remove the batch dimension.
+    # We're only interested in the first num_detections.
+    num_detections = int(detections.pop('num_detections'))
+    detections = {key: value[0, :num_detections].numpy()
+                   for key, value in detections.items()}
+    detections['num_detections'] = num_detections
+
+    # detection_classes should be ints.
+    detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+    #print(detections)
+    image_np_with_detections = image_np.copy()
+
+    viz_utils.visualize_boxes_and_labels_on_image_array(
+          image_np_with_detections,
+          detections['detection_boxes'],
+          detections['detection_classes'],
+          detections['detection_scores'],
+          category_index,
+          use_normalized_coordinates=True,
+          max_boxes_to_draw=boxes,
+          min_score_thresh=(min_score/100),
+          agnostic_mode=False)
+          
+    img = Image.fromarray(image_np_with_detections)
+    st.image(img,use_column_width=True)
+
+def run_faster_model_app(image_np,boxes,min_score):
+    show_inference(detection_model,image_np,boxes,min_score)
